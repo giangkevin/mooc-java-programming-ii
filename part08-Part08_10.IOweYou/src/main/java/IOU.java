c82460f4b87278939a6bd1f5241aee7343e90bf6
giangkevin <149978467+giangkevin@users.noreply.github.com>
2024-03-17 18:52:47 +0100

    Add part08-Part08_10.IOweYou/

---
+++ part08-Part08_10.IOweYou/src/main/java/IOU.java
@@ -0,0 +1,28 @@
+
+import java.util.HashMap;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class IOU {
+    private HashMap<String, Double> iou;
+    
+    public IOU(){
+        this.iou = new HashMap<>();
+    }
+    
+    public void setSum(String toWhom, double amount){
+        this.iou.put(toWhom, amount);
+    }
+    
+    public double howMuchDoIOweTo(String toWhom){
+        return this.iou.getOrDefault(toWhom, 0.0);
+    }
+}
